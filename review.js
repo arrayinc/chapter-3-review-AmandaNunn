8947183ed6c4337a2b3ab6ff1f6673982e7e001f
ET <et@arrayschool.com>
2018-12-09 13:20:20 -0700

    added review

---
+++ review.js
@@ -0,0 +1,50 @@
+// 1. What are the JavaScript keywords for the two Boolean values?
+
+
+// 2. How would you write a Boolean expression to test whether the value of the
+// integer variable n was in the range 0 to 9, inclusive?
+
+
+// 3. Describe in English what the following conditional expression means:
+// (x !== 4) || (x !== 17) For what values of x is this condition true?
+
+
+// 4. What is meant by the term short-circuit evaluation?
+
+
+// 5. What are the two classes of control statements?
+
+
+// 6. What does it mean to say that two control statements are nested?
+
+// 7. Describe in English the general operation of the switch statement.
+
+
+// 8. What rule does this chapter suggest with respect to the final statement in any
+// case or default clause?
+
+
+// 9. What special value is used in the monthName function in Figure 3-1 to indicate
+// an illegal numeric month?
+
+
+// 10. What was the nickname of the Small-Scale Experimental Machine developed at Manchester University that was in many respects the first modern digital computer?
+
+
+// 11. Suppose the body of a while loop contains a statement that, when executed, causes the condition for that while loop to become false. Does the loop terminate immediately at that point or does it complete the current cycle?
+
+
+// 12. What term do computer scientists use to refer to an incomplete program written partly in a programming language and partly in English?
+
+
+// 13. What is the purpose of each of the three expressions that appear in the control
+// line of a for statement?
+
+
+// 14. Whatforloopcontrollinewouldyouuseineachofthefollowingsituations:
+// a) Counting from 1 to 100.
+// b) Counting by sevens starting at 0 until the number has more than two digits.
+// c) Counting backward by twos from 100 to 0.
+
+
+// 15. What do the terms falsy and truthy signify in JavaScript? What strategy does this book suggest for avoiding the ambiguity associated with these terms?
